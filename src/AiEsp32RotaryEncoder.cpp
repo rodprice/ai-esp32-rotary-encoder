a5f1e9931157451eb79d7f113419857c1f706dc5
Rodney Price <rod@thirdoption.info>
2026-06-29 21:52:34 -0600

    Fix first encoder rotation so I responds right away

--- src/AiEsp32RotaryEncoder.cpp
+++ src/AiEsp32RotaryEncoder.cpp
@@ -190,7 +190,12 @@ void IRAM_ATTR AiEsp32RotaryEncoder::readButton_ISR()
 #endif
 }
 
-AiEsp32RotaryEncoder::AiEsp32RotaryEncoder(uint8_t encoder_APin, uint8_t encoder_BPin, int encoder_ButtonPin, int encoder_VccPin, uint8_t encoderSteps, bool areEncoderPinsPulldown_forEsp32)
+AiEsp32RotaryEncoder::AiEsp32RotaryEncoder(uint8_t encoder_APin,
+                                           uint8_t encoder_BPin,
+                                           int encoder_ButtonPin,
+                                           int encoder_VccPin,
+                                           uint8_t encoderSteps,
+                                           bool areEncoderPinsPulldown_forEsp32)
 {
 	this->old_AB = 0;
 
@@ -277,6 +282,8 @@ void AiEsp32RotaryEncoder::begin()
 		pinMode(this->encoderButtonPin, isButtonPulldown ? INPUT_PULLDOWN : INPUT_PULLUP);
 #endif
 	}
+        int8_t ENC_PORT = ((digitalRead(this->encoderBPin)) ? (1 << 1) : 0) | ((digitalRead(this->encoderAPin)) ? (1 << 0) : 0);
+        this->old_AB = (ENC_PORT & 0x03) | ((ENC_PORT & 0x03) << 2);
 }
 
 ButtonState AiEsp32RotaryEncoder::currentButtonState()
